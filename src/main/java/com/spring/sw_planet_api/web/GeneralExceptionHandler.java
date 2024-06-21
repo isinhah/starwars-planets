c4c3c1158da895d34dd1ec9b2bdf5bde88e955ca
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-06-21 08:17:39 -0300

    testes de integracao: cenario de planeta existente no controller

--- src/main/java/com/spring/sw_planet_api/web/GeneralExceptionHandler.java
+++ src/main/java/com/spring/sw_planet_api/web/GeneralExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.spring.sw_planet_api.web;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -17,5 +19,11 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
       HttpStatusCode status, WebRequest request) {
     return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  private ResponseEntity<Object> handleConflict(DataIntegrityViolationException ex) {
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body(ex.getMessage());
   }
 }
--- src/test/java/com/spring/sw_planet_api/domain/PlanetControllerTest.java
+++ src/test/java/com/spring/sw_planet_api/domain/PlanetControllerTest.java
@@ -2,15 +2,18 @@ package com.spring.sw_planet_api.domain;
 
 import static com.spring.sw_planet_api.common.PlanetConstants.INVALID_PLANET;
 import static com.spring.sw_planet_api.common.PlanetConstants.PLANET;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -55,5 +58,16 @@ public class PlanetControllerTest {
             post("/planets").content(objectMapper.writeValueAsString(INVALID_PLANET))
                 .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isUnprocessableEntity());
+  }
+
+  @Test
+  public void createPlanet_withExistingName_ReturnsConflict() throws Exception {
+    when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+
+    mockMvc
+            .perform(
+                    post("/planets").content(objectMapper.writeValueAsString(PLANET))
+                            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isConflict());
   }
 }
